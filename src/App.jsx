c646021bca409819b847f2144c77019105789153
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2023-12-26 01:24:48 +0100

    react router integration complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,41 @@
-import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React, { useState, useEffect, Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { getAllGames } from "./services/games";
+import { publicRoutes } from "./routes";
 
 function App() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    getGames()
+    getGames();
   }, []);
 
   const getGames = async () => {
     try {
-      const games =  await getAllGames();
-      console.log("games", games)
+      const games = await getAllGames();
+      console.log("games", games);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return (
     <>
-      <div className="flex flex-row justify-center">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>First Bank Academy Games</h1>
-      <div className="text-3xl">Coming Soon!!!</div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <React.StrictMode>
+        <Suspense>
+          <BrowserRouter>
+            <Routes>
+              {publicRoutes.map(page => {
+                const { id, path, Component } = page;
+                return (
+                  <Route key={id} exact path={path} element={<Component />} />
+                );
+              })}
+            </Routes>
+          </BrowserRouter>
+        </Suspense>
+      </React.StrictMode>
     </>
   );
 }
