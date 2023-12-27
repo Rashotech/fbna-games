e6d24193c38f086163b30e716556552215566b26
DavidAkabogu <Akabogudavid@gmail.com>
2023-12-27 16:19:35 +0100

    template building

--- src/pages/ProjectMembers.jsx
+++ src/pages/ProjectMembers.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const ProjectMembers = () => {
   return (
-    <div>ProjectMembers</div>
-  )
-}
+    // team members page 
+    <div className="items-center">
+      <div> Header </div>
+      {/* div containing intro statement */}
+      <div>
+        <h3>Meet the team...</h3>
+      </div>
+      {/* div containing images */}
+      <div>
+        <div className="flex">
+          <img src="./images/rasheed.jpeg" alt="image 1" />
+          <img src="./images/akabogu.png" alt="image 2" />
+          <img src="./images/habeeb.jpeg" alt="image 3" />
+        </div>
+        <div className="flex">
+          <img src="./images/oyin.jpeg" alt="image 4" />
+          <img src="./images/chisom.jpeg" alt="image 5" />
+          <img src="./images/" alt="image 6" />
+        </div>
+        <div className="flex">
+          <img src="./images/" alt="image 7" />
+          <img src="./images/" alt="image 8" />
+          <img src="./images/" alt="image 9" />
+        </div>
+      </div>
+      <div> Footer </div>
+    </div>
+  );
+};
 
-export default ProjectMembers
+export default ProjectMembers;
