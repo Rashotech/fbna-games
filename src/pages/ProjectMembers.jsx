0f74b91f8e2e2c0e758ca1bd28f587e91b4ff09a
DavidAkabogu <Akabogudavid@gmail.com>
2023-12-28 15:34:26 +0100

    align text

--- src/pages/ProjectMembers.jsx
+++ src/pages/ProjectMembers.jsx
@@ -61,14 +61,21 @@ const ProjectMembers = () => {
         <p className="font-bold text-blue-900 text-4xl ">Meet the Team...</p>
         <div className="flex flex-wrap mt-8 mx-16 font-bold">
           {teamMembers.map((member) => (
-            <div key={member.id} className="md:w-1/1 lg:w-1/2 xl:w-1/3 p-2 ">
+            <div
+              key={member.id}
+              className="mb-4 md:w-1/1 lg:w-1/2 xl:w-1/3 p-2 "
+            >
               <img
                 className="object-cover h-64 w-52 mb-1 ml-6 mx-4 rounded-md"
                 src={member.imageUrl}
                 alt={`image ${member.id}`}
               />
-              <p className="text-blue-900">{member.name}</p>
-              <p className="text-xs font-sans text-yellow-500">{member.role}</p>
+              <div className=" pl-6 text-start">
+                <p className="text-blue-900">{member.name}</p>
+                <p className="text-xs font-sans text-yellow-500">
+                  {member.role}
+                </p>
+              </div>
             </div>
           ))}
         </div>
