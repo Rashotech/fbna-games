cca70bd5fd64e6d49afcac57e13a643d0f8d085e
chisom promise <chimisepro@gmail.com>
2023-12-28 13:13:16 +0100

    fix: update card btn to use a Link component

--- src/components/GamesCardBtn.jsx
+++ src/components/GamesCardBtn.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const GamesCardBtn = ({ children, href }) => {
   return (
-    <a
-      href={href}
+    <Link
+      to={href}
       className="inline-flex items-center justify-center py-2 px-6 rounded-lg bg-secondary text-white text-sm font-bold"
     >
       {children}
-    </a>
+    </Link>
   );
 };
 
