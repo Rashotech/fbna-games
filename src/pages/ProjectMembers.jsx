9305c98327c297c28dd06e627ad146833aefc28e
DavidAkabogu <Akabogudavid@gmail.com>
2023-12-29 20:04:17 +0100

    team card component

--- src/pages/ProjectMembers.jsx
+++ src/pages/ProjectMembers.jsx
@@ -1,8 +1,11 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState } from "react";
 import { getProjectMembers } from "../services/teams";
+import TeamCard from "../components/TeamCard";
 
 const ProjectMembers = () => {
   const [teamMembers, setTeamMembers] = useState([]);
+  // eslint-disable-next-line no-unused-vars
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -11,18 +14,17 @@ const ProjectMembers = () => {
 
   const getTeamMembers = async () => {
     try {
-      setLoading(true)
+      setLoading(true);
       const members = await getProjectMembers();
       setTeamMembers(members);
-      setLoading(false)
+      setLoading(false);
     } catch (error) {
-      setLoading(false)
+      setLoading(false);
       console.log(error);
     }
   };
 
   return (
-    // team members page
     <div className="flex items-center bg-white">
       <div>
         <p className="font-bold text-blue-900 text-4xl mt-8 ">
@@ -30,22 +32,7 @@ const ProjectMembers = () => {
         </p>
         <div className="flex flex-wrap mt-8 mx-16 font-bold">
           {teamMembers.map((member) => (
-            <div
-              key={member.id}
-              className="mb-4 md:w-1/1 lg:w-1/2 xl:w-1/3 p-2 "
-            >
-              <img
-                className="object-cover h-64 w-52 mb-1 ml-6 mx-4 rounded-t-md border border-black-100"
-                src={member.image}
-                alt={`image ${member.id}`}
-              />
-              <div className="ml-6  text-start">
-                <p className="text-blue-900">{member.name}</p>
-                <p className="text-xs font-sans text-yellow-500">
-                  {member.role}
-                </p>
-              </div>
-            </div>
+            <TeamCard key={member.id} member={member} />
           ))}
         </div>
       </div>
