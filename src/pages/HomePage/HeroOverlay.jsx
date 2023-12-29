6f42119aa72ae95917be5017d02ba42b64ce35fb
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2023-12-29 03:25:01 +0100

    fix: resolve style issue with hero section slider

--- src/pages/HomePage/HeroOverlay.jsx
+++ src/pages/HomePage/HeroOverlay.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React from "react";
+import IconButton from "../../components/IconButton";
+import ExploreIcon from "../../components/icons/ExploreIcon";
+import { useNavigate } from "react-router-dom";
 
 const HeroOverlay = () => {
-    return (
-      <section className="hero_overlay text-[#022E64] px-4 py-10">
-        <h1 className="text-xl text-white font-bold leading-3">
-          Welcome To FirstAcademy Games
-        </h1>
-        <p className='text-white text-center'>
-          Visby is a geometric typeface that was influenced by the Arctic's
-          stark elegance a nd crisp air. In lowercase, it's friendly and
-          charismatic; in uppercase, it's elegant and authoritative. Humanist
-          nuances bring warmth to the design, which combines hard lines and
-          sharp corners with sleek, rounded letterforms.
-        </p>
-      </section>
-    );
-}
+  const navigate = useNavigate();
+  const exploreGames = () => {
+    navigate("games");
+  };
+  return (
+    <section className="hero_overlay text-start text-[#022E64] px-4 py-10 md:px-10 md:py-14 xl:px-20 xl:py-24">
+      <h1 className="w-[70%] text-xl text-white font-bold leading-8 mb-6 sm:text-3xl sm:mb-10 lg:text-4xl xl:text-5xl xl:w-[80%]">
+        Welcome To FirstAcademy Games
+      </h1>
+      <p className="max-w-[725px] text-white text-sm mb-6 leading-7 sm:w-[70%] sm:text-md sm:mb-10 lg:text-lg lg:w-[64%]">
+        Visby is a geometric typeface that was influenced by the Arctic's stark
+        elegance a nd crisp air. In lowercase, it's friendly and charismatic; in
+        uppercase, it's elegant and authoritative. Humanist nuances bring warmth
+        to the design, which combines hard lines and sharp corners with sleek,
+        rounded letterforms.
+      </p>
+      <IconButton
+        text={"Explore the Games"}
+        icon={<ExploreIcon />}
+        onClick={exploreGames}
+      />
+    </section>
+  );
+};
 
-export default HeroOverlay
+export default HeroOverlay;
