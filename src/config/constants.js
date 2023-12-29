069eef583ad9bfeae91cd7b78db5809cb8987650
Rasheed Ayoade <rashotech@gmail.com>
2023-12-29 18:26:50 +0100

    feat: add gallery

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,4 @@
 export const GAMES_COLLECTION = "games";
 export const PROJECT_MEMBERS_COLLECTION = "project-members";
 export const FEATURED_PLAYERS_COLLECTION = "featured-players";
+export const GALLERY_COLLECTION = "gallery";
