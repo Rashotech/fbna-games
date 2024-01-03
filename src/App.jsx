55d9f79d853a982740d97a675859bdd9d83d170a
chisom promise <chimisepro@gmail.com>
2024-01-03 06:30:27 +0100

    feat: add error boundary for error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
-import React, {  Suspense } from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { publicRoutes } from "./routes";
 import Layout from "./components/Layout";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   return (
-    <>
-      <React.StrictMode>
-        <Suspense>
-          <BrowserRouter>
-            <Routes>
-              <Route element={<Layout />}>
-                {publicRoutes.map(page => {
-                  const { id, path, Component } = page;
-                  return (
-                    <Route key={id} exact path={path} element={<Component />} />
-                  );
-                })}
-              </Route>
-            </Routes>
-          </BrowserRouter>
-        </Suspense>
-      </React.StrictMode>
-    </>
+    <ErrorBoundary>
+      <Suspense>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Layout />}>
+              {publicRoutes.map((page) => {
+                const { id, path, Component } = page;
+                return (
+                  <Route key={id} exact path={path} element={<Component />} />
+                );
+              })}
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
+    </ErrorBoundary>
   );
 }
 
--- src/components/FetchError.jsx
+++ src/components/FetchError.jsx
@@ -1,10 +1,22 @@
+import { cn } from "../utils";
+
 const reload = () => {
   typeof window !== "undefined" && window.location.reload();
 };
 
-const FetchError = ({ error, className, onRefetch = reload }) => {
+const FetchError = ({
+  error,
+  className,
+  onRefetch = reload,
+  buttonText = "Retry Again",
+}) => {
   return (
-    <div className="h-[50vh] flex flex-col items-center justify-center space-y-4 py-2 md:h-[40vh]">
+    <div
+      className={cn(
+        "h-[50vh] flex flex-col items-center justify-center space-y-4 py-2 md:h-[40vh]",
+        className
+      )}
+    >
       <p className="text-grey-600 text-base text-center font-medium md:text-xl">
         {error}
       </p>
@@ -12,7 +24,7 @@ const FetchError = ({ error, className, onRefetch = reload }) => {
         className="px-4 py-2 rounded-md inline-block bg-primary text-grey-100 focus:outline-none transition-colors hover:bg-primary/70"
         onClick={onRefetch}
       >
-        Retry Again
+        {buttonText}
       </button>
     </div>
   );
