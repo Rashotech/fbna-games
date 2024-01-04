bd6162068810cab95ab61950fc873dc2932b5af8
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2024-01-04 22:19:34 +0100

    feat: games of the week fully integrated

--- src/pages/HomePage/Upcoming/Games.jsx
+++ src/pages/HomePage/Upcoming/Games.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import GamesCard from "./GamesCard";
+import GamesCardSkeleton from "./GamesCardSkeleton";
 import { games } from "../../../assets/data";
 
-const Games = () => {
+const Games = ({ games, isLoading, error }) => {
   return (
     <div>
-      {games.map(game => {
-        const { id, name, date } = game;
-        return <GamesCard key={id} name={name} date={date} />;
-      })}
+      {isLoading && (
+        <div className="w-full mx-auto flex justify-center flex-wrap gap-4 md:w-[87%] lg:w-full lg:gap-3 xl:gap-6">
+          {Array.from({ length: 3 }, () => null).map((arr, i) => {
+            return <GamesCardSkeleton key={i} />;
+          })}
+        </div>
+      )}
+      {games.length == 0 && <div className="max-w-[200px] mx-auto text-herobg font-bold text-center"> Stay tuned for the games of the week...</div>}
+      {games.length > 0 &&
+        games?.map(game => {
+          const { id, name, startDate } = game;
+          return <GamesCard key={id} name={name} date={startDate} />;
+        })}
     </div>
   );
 };
--- src/pages/HomePage/Upcoming/Upcoming.jsx
+++ src/pages/HomePage/Upcoming/Upcoming.jsx
@@ -1,21 +1,54 @@
-import React from "react";
+import React, { useMemo, useEffect, useState } from "react";
 import SportDay from "./SportDay";
 import Games from "./Games";
+// import useFetcher from "../../../hooks/useFetcher";
+import { generateDates } from "../../../hooks/getStartAndEndDate";
+import { getGamesByDateRange } from "../../../services/games";
+// import { hasData } from "../../../utils";
+// import FetchError from "../../../components/FetchError";
+// import NotFound from "../../../components/NotFound";
 
 const Upcoming = () => {
+  const [data, setData] = useState({
+    games: [],
+    isLoading: true,
+    error: false,
+  });
+  const { games, isLoading, error } = data;
+  useEffect(() => {
+    const today = new Date();
+    const { start, end } = generateDates(today, 7);
+    let args = ["10-12-2023", "23-12-2023"];
+
+    const queryData = async () => {
+      try {
+        const data = await getGamesByDateRange(...args);
+        let result = { games: data, isLoading: false, error: false };
+        setData(result);
+      } catch (error) {
+        let result = { games: [], isLoading: false, error: true };
+        setData(result);
+      } finally {
+      }
+    };
+
+    queryData();
+  }, []);
   return (
     <section className="w-[90%] mx-auto">
-      <h1 className="text-center text-herobg text-2xl font-bold my-10">Upcoming Events</h1>
-      <div className="md:flex gap-10 max-w-[1300px] mx-auto">
-        <div className="basis-[60%]">
+      <h1 className="text-center text-herobg text-2xl font-bold my-10">
+        Upcoming Events
+      </h1>
+      <div className="md:flex justify-between gap-10 max-w-[1300px] mx-auto">
+        <div className="basis-[60%] lg:basis-[80%]">
           <SportDay />
         </div>
-        <div className="basis-[35%]">
+        <div className="basis-[40%] lg:basis-[20%]">
           <h4 className="text-xl text-btnbg text-center font-bold my-4">
             Games of the week
           </h4>
           <div>
-            <Games />
+            <Games games={games} isLoading={isLoading} error={error} />
           </div>
         </div>
       </div>
